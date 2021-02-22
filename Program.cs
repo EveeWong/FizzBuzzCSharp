70b170c6ac89d9f99fcecdbaacb0e6e60e87966f
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-02-22 12:00:39 +0000

    Added Fizz rule as a function

--- Program.cs
+++ Program.cs
@@ -5,10 +5,20 @@ namespace FizzBuzzC_
     class Program
     {
         static void Main(string[] args)
-        {
-        for (int i = 1; i <= 100; i++) {
-            Console.WriteLine(i);
-        }
+            {
+            for (int i = 1; i <= 100; i++) {      
+                 if (i % 3 == 0) {
+                 Console.WriteLine(PrintFizz());          
+                 } else { 
+                 Console.WriteLine(i);
+                 }             
+            }   
+            }
+        
+        static String PrintFizz() 
+            {
+         return "Fizz";
         }
     }
+
 }
