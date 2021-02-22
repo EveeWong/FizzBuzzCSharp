af19af0b77d053d51dfd7d12777f61af87ec1a76
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-02-22 11:19:36 +0000

    Prints 1-100

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace FizzBuzzC_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        for (int i = 1; i <= 100; i++) {
+            Console.WriteLine(i);
+        }
         }
     }
 }
