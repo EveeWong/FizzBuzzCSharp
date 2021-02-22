1277dd0f40fc7d98660ab7fd1906d9b390dbc674
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-02-22 12:05:51 +0000

    Added buzz rule as a function

--- Program.cs
+++ Program.cs
@@ -7,9 +7,13 @@ namespace FizzBuzzC_
         static void Main(string[] args)
             {
             for (int i = 1; i <= 100; i++) {      
-                 if (i % 3 == 0) {
-                 Console.WriteLine(PrintFizz());          
-                 } else { 
+                 if (i % 3 == 0 && i % 5 == 0) {
+                 Console.WriteLine(PrintFizz()+PrintBuzz());          
+                 } else if (i % 3 == 0) {
+                 Console.WriteLine(PrintFizz());
+                 } else if (i % 5 ==0) {
+                 Console.WriteLine(PrintBuzz());    
+                 } else {
                  Console.WriteLine(i);
                  }             
             }   
@@ -17,8 +21,13 @@ namespace FizzBuzzC_
         
         static String PrintFizz() 
             {
-         return "Fizz";
-        }
+            return "Fizz";
+            }
+
+        static String PrintBuzz()
+            {
+            return "Buzz";
+            }
     }
 
 }
